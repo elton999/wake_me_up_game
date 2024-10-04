3976cd79f888f972bf719beadca417961b67a23c
Elton Silva <eltoncarsilva73@gmail.com>
2024-10-04 03:24:18 -0300

    created scene and entity class

---
+++ 3dAnimation/Game3D/Entity.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game3D;
+
+public class Entity
+{
+    public Model Model;
+    public Texture2D Texture;
+    public Effect Effect;
+    public Vector3 Position = new Vector3(0, 0, 0);
+    public Matrix World;
+
+    private Scene _scene;
+    public Scene Scene => _scene;
+
+    public virtual void Start()
+    {
+        Effect = Scene.Content.Load<Effect>("shaders/DiffuseLighting");
+    }
+
+    public virtual void Update(float deltaTime) { }
+
+    public void UpdateAnimation(float deltaTime) { }
+
+    public void UpdateTransforms()
+    {
+        World = Matrix.CreateTranslation(Position);
+    }
+
+    public virtual void Draw(Matrix view, Matrix projection)
+    {
+        foreach (ModelMesh mesh in Model.Meshes)
+        {
+            foreach (ModelMeshPart meshPart in mesh.MeshParts)
+            {
+                meshPart.Effect = Effect;
+
+                Effect.Parameters["World"].SetValue(World);
+                Effect.Parameters["View"].SetValue(view);
+                Effect.Parameters["Projection"].SetValue(projection);
+
+                Effect.Parameters["lightPosition"].SetValue(Vector3.One * 1000);
+                Effect.Parameters["lightColor"].SetValue(Color.White.ToVector4());
+                Effect.Parameters["lightIntensity"].SetValue(1.0f);
+                Effect.Parameters["SpriteTexture"].SetValue(Texture);
+            }
+            mesh.Draw();
+        }
+    }
+
+    public void AddScene(Scene scene) => _scene = scene;
+}
---
+++ 3dAnimation/Game3D/Scene.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game3D;
+
+public class Scene
+{
+    private List<Entity> entities = new();
+
+    public Camera Camera;
+    public ContentManager Content;
+
+    public Scene(GraphicsDevice gpu, ContentManager content)
+    {
+        Camera = new(gpu, Vector3.UnitZ);
+        Content = content;
+    }
+
+    public void AddEntity(Entity entity)
+    {
+        entity.AddScene(this);
+        entity.Start();
+        entities.Add(entity);
+    }
+
+    public void Update(float deltaTime)
+    {
+        foreach (var entity in entities)
+        {
+            entity.Update(deltaTime);
+            entity.UpdateAnimation(deltaTime);
+            entity.UpdateTransforms();
+        }
+    }
+
+    public void Draw()
+    {
+        foreach (var entity in entities)
+        {
+            entity.Draw(Camera.View, Camera.Projection);
+        }
+    }
+}
