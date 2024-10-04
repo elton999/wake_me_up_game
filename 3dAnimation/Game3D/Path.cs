326f94b4bfe8bcf234780dc1bc22c043d2d8eb7c
Elton Silva <eltoncarsilva73@gmail.com>
2024-10-04 20:33:46 -0300

    added solid color texture

--- 3dAnimation/Game3D/Path.cs
+++ 3dAnimation/Game3D/Path.cs
@@ -7,7 +7,9 @@ public static class Path
     public const string MODELS_PATH = "models/";
 
     public const string BASIC_SHADERS_PATH = SHADERS_PATH + "DiffuseLighting";
+
     public const string BASIC_TEXTURE_PATH = TEXTURES_PATH + "default_texture";
+    public const string SOLID_COLOR_TEXTURE_PATH = TEXTURES_PATH + "default_white";
 
     public const string MODEL_HOUSE_PATH = MODELS_PATH + "house";
 
