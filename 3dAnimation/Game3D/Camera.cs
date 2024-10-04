e31b83c8ae2d48572f042072c8305c7033fa5101
Elton Silva <eltoncarsilva73@gmail.com>
2024-10-04 03:23:55 -0300

    camera minor refactoring

--- 3dAnimation/Game3D/Camera.cs
+++ 3dAnimation/Game3D/Camera.cs
@@ -1,45 +1,37 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Game3D
+namespace Game3D;
+public class Camera
 {
-    public  class Camera
+    public const float FAR_PLANE = 5000;
+
+    public Vector3 Position, Target;
+    public Matrix View, Projection, ViewProjection;
+    public Vector3 Up;
+
+    public Camera(GraphicsDevice gpu, Vector3 UpDirection)
     {
-        public float CAM_HEIGHT = 12;
-        public float HEAD_OFFSET = 12;
-        public const float FAR_PLANE = 2000;
+        Up = UpDirection;
+        Position = new Vector3(20.0f, 12.0f, 10.0f);
+        Target = Vector3.One;
 
-        public Vector3 pos, target;
-        public Matrix view, proj, view_proj;
-        public Vector3 up;
-        float current_angle = 45.0f;
-        float angle_velocity;
-        float radius = 100.0f;
-        Vector3 unit_direction;
+        View = Matrix.CreateLookAt(Position, Target, Up);
+        Projection = Matrix.CreatePerspectiveFieldOfView(/*MathHelper.PiOver4*/ MathHelper.ToRadians(45), gpu.Viewport.AspectRatio, 0.01f, FAR_PLANE);
+        ViewProjection = View * Projection;
+    }
 
-        public Camera(GraphicsDevice gpu, Vector3 UpDirection)
-        {
-            up = UpDirection;
-            pos = new Vector3(20,12, -90);
-            target = Vector3.Zero;
+    public void MoveCamera(Vector3 move)
+    {
+        Position += move;
+        View = Matrix.CreateLookAt(Position, Target, Up);
+        ViewProjection = View * Projection;
+    }
 
-            view = Matrix.CreateLookAt(pos, target, up);
-            proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, gpu.Viewport.AspectRatio, 1.0f, FAR_PLANE);
-            view_proj = view * proj;
-        }
-
-        public void MoveCamera(Vector3 move)
-        {
-            pos += move;
-            view = Matrix.CreateLookAt(pos, target, up);
-            view_proj = view * proj;
-        }
-
-        public void UpdateTarget(Vector3 new_target)
-        {
-            target = new_target;
-            view = Matrix.CreateLookAt(pos, target, pos);
-            view_proj = view * proj;
-        }
+    public void UpdateTarget(Vector3 new_target)
+    {
+        Target = new_target;
+        View = Matrix.CreateLookAt(Position, Target, Position);
+        ViewProjection = View * Projection;
     }
 }
