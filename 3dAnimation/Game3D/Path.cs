28e878de9f8dcf0f70771fef1660ad1dc651a36e
Elton Silva <eltoncarsilva73@gmail.com>
2024-10-20 17:13:05 -0300

    paths for score text

--- 3dAnimation/Game3D/Path.cs
+++ 3dAnimation/Game3D/Path.cs
@@ -18,6 +18,11 @@ public static class Path
     public const string RIGHT_TEXTURE_PATH = UI_TEXTURES_PATH + "right";
     public const string UI_CHAR_TEXTURE_PATH = UI_TEXTURES_PATH + "char_ui";
 
+    public const string PERFECT_TEXTURE_PATH = UI_TEXTURES_PATH + "perfect_text";
+    public const string GOOD_TEXTURE_PATH = UI_TEXTURES_PATH + "good_text";
+    public const string OK_TEXTURE_PATH = UI_TEXTURES_PATH + "not_bad_text";
+    public const string WRONG_TEXTURE_PATH = UI_TEXTURES_PATH + "wrong_text";
+
     public const string MODEL_HOUSE_PATH = MODELS_PATH + "house";
     public const string MODEL_NPC_PATH = MODELS_PATH + "default_npc";
 
