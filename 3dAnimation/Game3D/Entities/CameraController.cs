485f1880cae59ea451c66dbd6a43fb7a41b2c664
Elton Silva <eltoncarsilva73@gmail.com>
2024-11-09 12:35:03 -0300

    integration of game states on camera animation

--- 3dAnimation/Game3D/Entities/CameraController.cs
+++ 3dAnimation/Game3D/Entities/CameraController.cs
@@ -1,36 +1,23 @@
 using System;
-using Game3D.UI;
 using Microsoft.Xna.Framework;
 
 namespace Game3D.Entities;
 
 public class CameraController : UIEntity
 {
-    public enum CameraAnimation
-    {
-        Default,
-        EndLevel,
-    }
-
-    public CameraAnimation CurrentAnimation = CameraAnimation.Default;
-
     public float Speed = 10.0f;
     public Vector3 CameraDefaultPosition = new Vector3(14.5f, 6.8f, 7.0f);
 
-    public override void Start()
-    {
-        SequenceTimeLineUI.OnFinishEvent += EndLevel;
-        StartLevel();
-    }
+    public override void Start() => StartLevel();
 
     public override void Update(float deltaTime)
     {
-        if(CurrentAnimation == CameraAnimation.Default)
+        if(GameStates.CurrentState == GameStates.State.PLAYING)
         {
-
+            StartLevel();
         }
 
-        if(CurrentAnimation == CameraAnimation.EndLevel)
+        if(GameStates.CurrentState == GameStates.State.END_LEVEL)
         {
             var cameraPosition = Scene.Camera.Position;
             float xValue = Math.Clamp(cameraPosition.X - Speed * deltaTime , 0.0f, CameraDefaultPosition.X);
@@ -39,15 +26,8 @@ public class CameraController : UIEntity
         }
     }
 
-
-    public void EndLevel()
-    {
-        CurrentAnimation = CameraAnimation.EndLevel;
-    }
-
     public void StartLevel()
     {
-        CurrentAnimation = CameraAnimation.Default;
         Scene.Camera.SetPosition(CameraDefaultPosition);
     }
 
